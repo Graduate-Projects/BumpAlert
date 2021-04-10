caf0628974d0ee2d30dd469ec632ae9d5f0c971b
Ala' Jaber <Alaa@dev.gsoft.sa>
2021-04-10 15:20:42 +0300

    Add project files.

---
+++ Bump/Bump/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Bump
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int count = 0;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"your clicked {count} items";
+        }
+    }
+}
