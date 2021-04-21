97a65508d3342a687766d162ae66f686f2ed37a9
Ala' Jaber <Alaa@dev.gsoft.sa>
2021-04-21 15:40:40 +0300

    -create heba branch

--- Bump/Bump.Android/MainActivity.cs
+++ Bump/Bump.Android/MainActivity.cs
@@ -20,7 +20,7 @@ namespace Bump.Droid
 
             Xamarin.FormsGoogleMaps.Init(this, savedInstanceState);
 
-            LoadApplication(new App());
+            LoadApplication(new App()); 
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
