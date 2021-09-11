c0854e5042580ce04b866e3c7da3679ac0a2bbb3
RAGHAD ALKHATEEB <rkalkhateeb17@cit.just.edu.jo>
2021-09-11 18:32:34 +0300

    ContactUs links

---
+++ Bump/Bump/ViewModels/ContributeFlyout.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Bump.ViewModels
+{
+    class ContributeFlyout : BaseViewModel
+    {
+        public ICommand OpenMainPageCommand { get; set; }
+
+        public ContributeFlyout()
+        {
+            OpenMainPageCommand = new Command(OpenMainPage);
+        }
+
+        private void OpenMainPage()
+        {
+            OpenPage(new Views.MainPageView.MainPage());
+        }
+    }
+}
